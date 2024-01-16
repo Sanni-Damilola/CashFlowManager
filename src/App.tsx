601457ca6c11fa95baa2fdd874925849b714914e
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-16 02:02:39 +0100

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { useState, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { Routes } from "./routes";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { handleNavigateBack } from "./utils/Utils";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
   return (
-    <div>
-      <div onClick={handleNavigateBack} className="back">
-        <IoMdArrowRoundBack />
-      </div>
-      <RouterProvider router={Routes()} />
+    <div style={{ position: "relative" }}>
+      {loading && (
+        <div className="loading-overlay">
+          <div className="loading-spinner" />
+        </div>
+      )}
+
+      {!loading && (
+        <div onClick={handleNavigateBack} className="back">
+          <IoMdArrowRoundBack />
+        </div>
+      )}
+
+      {!loading && <RouterProvider router={Routes()} />}
     </div>
   );
 };
