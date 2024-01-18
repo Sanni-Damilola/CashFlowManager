d3df7ca0a54362b4b136f17522f1c0cec6cd0b2d
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-18 21:24:57 +0100

    added

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,11 @@ import { Routes } from "./routes";
 import { Footer } from "./static";
 
 const App = () => {
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const loadingTimeout = setTimeout(() => {
-  //     setLoading(false);
-  //   }, 2000);
-
-  //   return () => clearTimeout(loadingTimeout);
-  // }, []);
-
   return (
     <div style={{ position: "relative" }}>
-      {/* {!loading && ( */}
       <Suspense fallback={<div>Loading...</div>}>
         <RouterProvider router={Routes()} />
       </Suspense>
-      {/* )} */}
-
       <Footer />
     </div>
   );
